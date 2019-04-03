354e5ac1365efc5262b0f928bdbd5bad0a8c100a
morris <zorzi.morris@iisgalvanimi.edu.it>
2019-04-03 10:56:25 +0000

    Es. Aws

--- app/app.js
+++ app/app.js
@@ -1,10 +1,32 @@
-/*eslint-env node*/
-
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res){
+    res.render('home')
+});
+
+app.get('/risultato', function (req, res) {
+    var conn = mysql.createConnection({
+        host: 'mysql-instance1.cedlzfy3hode.us-east-1.rds.amazonaws.com',
+        user: 'zorzi_morris',
+        password: 'LeoMessi10#',
+        database: 'zorziDB'
+    });
+    conn.connect(function(err){
+        if(err) 
+        throw err;
+        conn.query("SELECT * FROM utente", function(err, result, fields){
+            if(err)
+            throw err;
+            console.log(result);
+            res.render('risultato',{list:result});
+        });
+        
+    });
 });
 
 app.listen(3000, function () {
